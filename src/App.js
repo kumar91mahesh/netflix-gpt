10f0f2bfb14a7baa45d3157429400afbcd053d57
mahesh kumar <maheshkumar@maheshs-MacBook-Pro.local>
2025-03-02 22:06:26 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
-  return (
-    <div className="text-3xl font-bold text-green-500">
-      Namste everyone , lets build a react app
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
